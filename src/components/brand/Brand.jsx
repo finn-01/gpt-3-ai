0744731074821cf2d6f61da9d058cd3635a41cae
finn-01 <nguyensam576@gmail.com>
2022-01-04 13:38:09 +0700

    brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+
+const socials = [google, slack, atlassian, dropbox, shopify];
 
 const Brand = () => {
-	return <div>Brand</div>;
+	return (
+		<div className="gpt3__brand section__padding">
+			{socials.map((social) => (
+				<div key={social}>
+					<img src={social} alt={social} />
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Brand;
